a405b4cb4be440ce53f6aa60130b95738e28e165
cheimachiha <cheimachiha@gmail.com>
2025-01-24 11:52:58 +0100

    authentification

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -3,14 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserAuthController extends Controller
 {
     //
-    function login(Request $request){
-        return"login function";
+    function login(Request $request)
+    {
+        $input = $request->all();
+
+        // Validation des champs requis
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        // Vérifie si l'utilisateur existe
+        $user = User::where('email', $input['email'])->first();
+
+        if (!$user || !Hash::check($input['password'], $user->password)) {
+            return response()->json(['msg' => 'Invalid credentials'], 401);
+        }
+
+        // Génération d'un token d'accès
+        $success['token'] = $user->createToken('MyApp')->plainTextToken;
+        $success['name'] = $user->name;
+
+        return response()->json(['result' => $success, 'msg' => 'Login successful']);
+    }
+    function logout(Request $request)
+    {
+        // Suppression du token actuel
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['msg' => 'Logout successful']);
     }
     function signup(Request $request){
-        return"signup function";
+        $input =$request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token']= $user->createToken('MyApp')->plainTextToken;
+        $success['name']= $user->name;
+        return ['result'=> $success, 'msg'=>"user register successfully"];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuthController;
 
-Route::post("login",[UserAuthController::class ,"login"]);
-Route::post("signup",[UserAuthController::class ,"signup"]);
+
+// Routes d'authentification
+Route::post('/signup', [UserAuthController::class, 'signup']);
+Route::post('/login', [UserAuthController::class, 'login']);
+Route::post('/logout', [UserAuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();

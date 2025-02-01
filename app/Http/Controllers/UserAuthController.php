d02e437e8672354da527643e1b831e3abb6b5f92
cheimachiha <cheimachiha@gmail.com>
2025-02-01 10:27:13 +0100

    ClassProduct

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -11,103 +11,108 @@ use Illuminate\Support\Facades\Password;
 
 class UserAuthController extends Controller
 {
-    //
+    function signup(Request $request){
+    //validation?? email
+            $input =$request->all();
+
+            //min6car?
+            $input['password'] = bcrypt($input['password']);
+            $user = User::create($input);
+            $success['token']= $user->createToken('MyApp')->plainTextToken;
+            //non caratc sp
+            $success['name']= $user->name;
+            return ['result'=> $success, 'msg'=>"user register successfully"];
+        }
     public function login(LoginRequest $request)
-{
-    $input = $request->validated(); // Récupère uniquement les données validées
+        {
+            $input = $request->validated(); // Récupère uniquement les données validées
 
-    // Vérifie si l'utilisateur existe
-    $user = User::where('email', $input['email'])->first();
+            // Vérifie si l'utilisateur existe
+            $user = User::where('email', $input['email'])->first();
 
-    if (!$user || !Hash::check($input['password'], $user->password)) {
-        return response()->json(['msg' => 'Invalid credentials'], 401);
-    }
+            if (!$user || !Hash::check($input['password'], $user->password)) {
+                return response()->json(['msg' => 'Invalid credentials'], 401);
+            }
 
-    // Génération d'un token d'accès
-    $success['token'] = $user->createToken('MyApp')->plainTextToken;
-    $success['name'] = $user->name;
+            // Génération d'un token d'accès
+            $success['token'] = $user->createToken('MyApp')->plainTextToken;
+            $success['name'] = $user->name;
 
-    return response()->json(['result' => $success, 'msg' => 'Login successful']);
-}
+            return response()->json(['result' => $success, 'msg' => 'Login successful']);
+        }
 
     function logout(Request $request)
-    {
-        // Suppression du token actuel
-        $request->user()->currentAccessToken()->delete();
+        {
+            // Suppression du token actuel
+            $request->user()->currentAccessToken()->delete();
 
-        return response()->json(['msg' => 'Logout successful']);
-    }
-    function signup(Request $request){
+            return response()->json(['msg' => 'Logout successful']);
+        }
 
-        $input =$request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token']= $user->createToken('MyApp')->plainTextToken;
-        $success['name']= $user->name;
-        return ['result'=> $success, 'msg'=>"user register successfully"];
-    }
 
 
     public function forgotPassword(Request $request){
 
-        // Validation de l'email
-        $request->validate([
-            'email' => 'required|email',
-        ]);
+            // Validation de l'email
+            $request->validate([
+                'email' => 'required|email',
+            ]);
 
-        // Envoi de l'e-mail de réinitialisation
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+            // Envoi de l'e-mail de réinitialisation
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
 
-        // Vérifie le statut et renvoie une réponse
-        if ($status === Password::RESET_LINK_SENT) {
-            return response()->json([
-                'message' => 'Reset password link sent successfully.',
-                'status' => __($status),
-            ], 200);
-        } else {
-            return response()->json([
-                'message' => 'Failed to send reset password link.',
-                'status' => __($status),
-            ], 400);
-        }
+
+
+            // Vérifie le statut et renvoie une réponse
+            if ($status === Password::RESET_LINK_SENT) {
+                return response()->json([
+                    'message' => 'Reset password link sent successfully.',
+                    'status' => __($status),
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Failed to send reset password link.',
+                    'status' => __($status),
+                ], 400);
+            }
 
      }
 
 
     public function resetPassword(Request $request)
-        {
-        // Validation des champs requis
-        $request->validate([
-            'token' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|min:8|confirmed', // Assurez-vous que le champ "password_confirmation" est inclus
-        ]);
+            {
+            // Validation des champs requis
+            $request->validate([
+                'token' => 'required',
+                'email' => 'required|email',
+                'password' => 'required|min:8|confirmed', // Assurez-vous que le champ "password_confirmation" est inclus
+            ]);
 
-        // Réinitialiser le mot de passe
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user, $password) {
-                $user->forceFill([
-                    'password' => Hash::make($password),
-                ])->save();
+            // Réinitialiser le mot de passe
+            $status = Password::reset(
+                $request->only('email', 'password', 'password_confirmation', 'token'),
+                function ($user, $password) {
+                    $user->forceFill([
+                        'password' => Hash::make($password),
+                    ])->save();
+                }
+            );
+
+            // Vérifie le statut et renvoie une réponse
+            if ($status === Password::PASSWORD_RESET) {
+                return response()->json([
+                    'message' => 'Password has been successfully reset.',
+                    'status' => __($status),
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Failed to reset password.',
+                    'status' => __($status),
+                ], 400);
             }
-        );
-
-        // Vérifie le statut et renvoie une réponse
-        if ($status === Password::PASSWORD_RESET) {
-            return response()->json([
-                'message' => 'Password has been successfully reset.',
-                'status' => __($status),
-            ], 200);
-        } else {
-            return response()->json([
-                'message' => 'Failed to reset password.',
-                'status' => __($status),
-            ], 400);
         }
-    }
 
 
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,38 @@
 <?php
 
-namespace App\Models;
 
+       namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Enums\Fit;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
-    //
+    use HasFactory, InteractsWithMedia;
+
+    protected $fillable = [
+        'name',
+        'refrence',
+        'description',
+        'price',
+        'stock',
+        'category_id',
+        'image',
+    ];
+
+    // Relation avec la catégorie
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+
+  
 }
+
+
+
